d9c843cce07dc856eb25b575c2d23df7623ba5f3
Surya Efendi <suryaefendi72@gmail.com>
2021-09-15 08:40:56 +0700

    refactoring flatten_a_list.py

--- py-principles/Flatten_a_list.py
+++ py-principles/Flatten_a_list.py
@@ -1,15 +1,5 @@
+def flatten(l: list[list[int]]) -> list:
+    return [hasil for loop in l for hasil in loop]
 
 
-def flatten(l):
-    finnal_list = []
-    for L in l:
-        if len(L) > 1:
-           for _l in L:
-               finnal_list.append(_l)
-        if len(L) == 1:
-           finnal_list.append(L[0])
-
-    return finnal_list
-
-
-print (flatten([[1],[2],[3],[4,5]]))
+print(flatten([[1], [2], [3], [4, 5]]))
